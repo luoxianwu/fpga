1aedfba89dc85609cfc7642ac7408cdd13cdd0f3
Luo.xianwu <x-luo@ispace-us.io>
2024-11-13 17:46:26 -0700

    Add port to SPI pins changed SPI ip core settings

--- HW/sge/bsp/driver/riscv_mc/util.c
+++ HW/sge/bsp/driver/riscv_mc/util.c
@@ -2,7 +2,7 @@
 
      >>>>>>>>>>>>>>>>>>>>>>> COPYRIGHT NOTICE <<<<<<<<<<<<<<<<<<<<<<<<<
      ------------------------------------------------------------------
-     Copyright (c) 2019-2024 by Lattice Semiconductor Corporation
+     Copyright (c) 2019-2020 by Lattice Semiconductor Corporation
      ALL RIGHTS RESERVED
      ------------------------------------------------------------------
 
